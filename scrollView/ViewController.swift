f94df746eb1d21b0c13bdda7ae0131dcb48a5b6f
Anurag Reddy <anuragreddy@Anurags-MacBook-Pro.local>
2020-07-27 13:53:42 +0530

    initial

---
+++ scrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrollView
+//
+//  Created by Anurag Reddy on 10/07/20.
+//  Copyright © 2020 Anurag Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
